6426b330bea644ff88c8bd29e5c6db434d8ed32f
Pyrrxa <pyrrxa@gmail.com>
2019-10-30 14:16:56 -0400

    fetchBooks.py bug fix

--- data/fetchBooks.py
+++ data/fetchBooks.py
@@ -11,22 +11,22 @@ with requests.Session() as s:
     for page in range(1, 10000):
         if page % 10 == 0:
             print(page)
-            url = (
-                "http://test.tarheelreader.org/find/"
-                "?search=&category=&reviewed=&audience=&language=&"
-                f"page={page}&json=1"
-            )
+        url = (
+            "http://test.tarheelreader.org/find/"
+            "?search=&category=&reviewed=&audience=&language=&"
+            f"page={page}&json=1"
+        )
+        resp = s.get(url)
+        r = resp.json()
+        for b in r["books"]:
+            if b["slug"] in books:
+                continue
+            url = "http://test.tarheelreader.org/book-as-json/" f'?slug={b["slug"]}'
             resp = s.get(url)
-            r = resp.json()
-            for b in r["books"]:
-                if b["slug"] in books:
-                    continue
-                url = "http://test.tarheelreader.org/book-as-json/" f'?slug={b["slug"]}'
-                resp = s.get(url)
-                book = resp.json()
-                books[b["slug"]] = book
-                if not r["more"]:
-                    break
+            book = resp.json()
+            books[b["slug"]] = book
+            if not r["more"]:
+                break
 
 rows = sorted(books.values(), key=lambda b: b["ID"])
 with gzip.open("books.json.gz", "wt", encoding="utf-8") as fp:
