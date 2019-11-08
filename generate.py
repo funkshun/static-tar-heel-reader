19b2b29ab9a2364258e9635558b0b50474c99774
Taylor Smith <tas12740@gmail.com>
2019-11-07 23:15:33 -0500

    Update generate Python file to include more print statements, be more readable, and have less import overhead

--- generate.py
+++ generate.py
@@ -3,23 +3,22 @@
 Experiment with allowing variable base
 """
 
-import gzip
-import json
+from gzip import open as gzip_open
+from json import load as json_load, dump as json_dump
 from mako.template import Template
 from mako import exceptions
-import os
+from os import makedirs
 import os.path as osp
-import itertools
-import shutil
-import re
-import requests
-from stemming.porter2 import stem
-import aspell
-#from spellchecker import SpellChecker
-import contractions
-import pandas as pd
+from itertools import groupby as itertools_groupby
+from shutil import copyfileobj
+from re import findall as regex_findall, IGNORECASE
+from requests import Session
+from nltk.stem.porter import PorterStemmer
+from spellchecker import SpellChecker
+from contractions import fix as contractions_fix
+from pandas import DataFrame
 import myArgs
-import math
+from math import ceil, log
 from sqlitedict import SqliteDict
 from copypage import CopyPage
 
@@ -32,6 +31,10 @@ args = myArgs.Parse(
     query="",
     hasCat=True,
     hasAudience=True,
+    lang="en",
+    minAppearances=2,
+    minWordsPerBook=8,
+    maxWordsPerBook=100,
     images="/archives/tarheelreader/production",
     books="data/books.json.gz",
 )
@@ -39,7 +42,7 @@ args = myArgs.Parse(
 cp = CopyPage()
 
 # get all the books
-books = json.load(gzip.open(args.books, "rt", encoding="utf-8"))
+books = json_load(gzip_open(args.books, "rt", encoding="utf-8"))
 
 
 def render(template, view):
@@ -61,12 +64,12 @@ def matchesQuery(book, query):
     )
 
 
-# get the English books that qualify
+# get the books that qualify
 books = [
     book
     for book in books
-    # is English
-    if book["language"] == "en"
+    # is the specified language (English default)
+    if book["language"] == args.lang
     # satisfies the query if any
     and (not args.query or matchesQuery(book, args.query))
     # is categorized
@@ -74,10 +77,10 @@ books = [
     # has an audience
     and (not args.hasAudience or book["audience"] in "EC")
     # has enough pages or is reviewed
-    and (args.minPages < len(book["pages"]) < args.maxPages or book["reviewed"])
+    and ((args.minPages < len(book["pages"]) < args.maxPages) or book["reviewed"])
 ]
 
-print(len(books))
+print('Number of books that qualify: ', len(books))
 
 # break into reviewed and unreviewed
 reviewed = [book for book in books if book["reviewed"]][: args.Nselect]
@@ -86,11 +89,10 @@ unreviewed = [book for book in books if not book["reviewed"]][: args.Nselect]
 selected = reviewed + unreviewed
 
 # activate the spell checkers
-spell = aspell.Speller("lang", "en")
-#spell2 = SpellChecker()
+spell = SpellChecker()
 
 
-def getWords(book):
+def getWords(book, stemmer):
     """Return words from the book
 
     replace contractions
@@ -99,20 +101,22 @@ def getWords(book):
     """
     words = []
     for page in book["pages"]:
-        text = contractions.fix(page["text"])
+        text = contractions_fix(page["text"])
         text = text.replace("'", "")
         words += [
-            stem(word).lower()
-            for word in re.findall(r"[a-z]+", text, re.I)
-            if spell.check(word) #or spell2.known([word])
+            stemmer.stem(word).lower()
+            for word in regex_findall(r"[a-z]+", text, flags=IGNORECASE)
+            if spell.known(words=[word])
         ]
     return set(words)
 
 
+stemmer = PorterStemmer()
+
 index = []
 for book in selected:
     slug = book["slug"]
-    words = getWords(book)
+    words = getWords(book, stemmer)
     for word in words:
         index.append((word, slug))
     for category in book["categories"]:
@@ -121,33 +125,43 @@ for book in selected:
         index.append(("CAUTION", slug))
 
 
-index = pd.DataFrame(index)
-index.columns = ["word", "slug"]
+index = DataFrame(index, columns=['word', 'slug'])
 
+print(f'(Words, Slugs): {index.shape}')
 
 # repeat because dropping some might change inclusion of others
-for i in range(4):
+slug_set = set(index.slug.unique())
+word_set = set(index.word.unique())
+i = 0
+while True:
     # drop the words that only occur a few times
     booksPerWord = index.groupby("word").slug.count()
-    index = index[index.word.isin(booksPerWord[booksPerWord > 2].index)]
+    index = index[index.word.isin(
+        booksPerWord[booksPerWord > args.minAppearances].index)]
 
     # drop the books that have too few or too many words
     wordsPerBook = index.groupby("slug").word.count()
-    rightSize = (wordsPerBook >= 8) & (wordsPerBook < 100)
+    rightSize = (wordsPerBook >= args.minWordsPerBook) & (
+        wordsPerBook < args.maxWordsPerBook)
     index = index[index.slug.isin(wordsPerBook[rightSize].index)]
 
+    new_slug_set = set(index.slug.unique())
+    new_word_set = set(index.word.unique())
+    if len(new_slug_set) == len(slug_set) and len(new_word_set) == len(word_set):
+        break
+    slug_set = new_slug_set
+    word_set = new_word_set
 
 # make the index from words to slugs
 wordToSlugs = index.groupby("word").slug.apply(list)
-
-
 slugs = index.slug.unique()
-
 
 # only keep the selected books for the rest of the processing
 books = [book for book in books if book["slug"] in slugs]
 Nbooks = len(books)
-Dbooks = int(math.ceil(math.log(Nbooks, args.base)))
+Dbooks = int(ceil(log(Nbooks, args.base)))
+
+print(f'Parsed out some books! Now have {Nbooks}, or {Dbooks} (in base {args.base})')
 
 # count the pictures
 pictures = set()
@@ -155,7 +169,9 @@ for book in books:
     for page in book["pages"]:
         pictures.add(page["url"])
 Npictures = len(pictures)
-Dpictures = int(math.ceil(math.log(Npictures, args.base)))
+Dpictures = int(ceil(log(Npictures, args.base)))
+
+print(f'Number of pictures: {Npictures}, or {Dpictures} (in base {args.base})')
 
 OUT = args.out
 CONTENT = osp.join(OUT, "content")
@@ -187,15 +203,16 @@ bookmap = {}
 def make_bookid(slug):
     """get unique id for a book"""
     if slug not in bookmap:
-        i = len(bookmap)
-        r = encode(i, Dbooks)
-        path = osp.join(CONTENT, *list(r)) + ".html"
-        bookmap[slug] = r, path
+        num_books = len(bookmap)
+        res_encode = encode(num_books, Dbooks)
+        # *list makes the file structure first_digit/second_digit/...last_digit.html
+        path = osp.join(CONTENT, *list(res_encode)) + ".html"
+        bookmap[slug] = (res_encode, path)
     return bookmap[slug]
 
 
 # map image URL to new name
-os.makedirs(OUT, exist_ok=True)
+makedirs(OUT, exist_ok=True)
 imagemap = SqliteDict(osp.join(OUT, "imagemap.sd"), autocommit=True)
 
 
@@ -204,14 +221,14 @@ def imgurl(url, bid, bpath, session):
     if url in imagemap:
         path = imagemap[url]
     else:
-        i = len(imagemap)
-        r = encode(i, Dpictures)
-        path = osp.join(CONTENT, *r) + ".jpg"
-        os.makedirs(osp.dirname(path), exist_ok=True)
+        num_pictures = len(imagemap)
+        res_encode = encode(num_pictures, Dpictures)
+        path = osp.join(CONTENT, *res_encode) + ".jpg"
+        makedirs(osp.dirname(path), exist_ok=True)
         resp = session.get("http://tarheelreader.org" + url, stream=True)
         with open(path, 'wb') as f:
             resp.raw.decode_content = True
-            shutil.copyfileobj(resp.raw, f)
+            copyfileobj(resp.raw, f)
         imagemap[url] = path
     return osp.relpath(path, osp.dirname(bpath))
 
@@ -220,10 +237,14 @@ def imgurl(url, bid, bpath, session):
 ndx = []
 template = open("src/book.mako").read()
 lastReviewed = None
+
+book_css = osp.join(OUT, cp.copy("book.css"))
+book_js = osp.join(OUT, cp.link("book.js"))
+
 for progress, book in enumerate(books):
-    if progress % 100 == 0:
-        print(progress)
     bid, bpath = make_bookid(book["slug"])
+    if progress % 100 == 0:
+        print(f"Template making progress: {progress}, book ID {bid}, book path {bpath}")
     icons = []
     if book["audience"] == "C":
         icons.append("C")
@@ -232,7 +253,7 @@ for progress, book in enumerate(books):
         lastReviewed = bid
     last = bid
     ipath = osp.join(osp.dirname(bpath), "index.html")
-    sess = requests.Session()
+    sess = Session()
     ndx.append(
         dict(
             title=book["title"],
@@ -245,7 +266,8 @@ for progress, book in enumerate(books):
             path=ipath,
         )
     )
-    view = dict(start="#" + make_pageid(1), title=book["title"], index=f"./#{bid}")
+    view = dict(start="#" + make_pageid(1),
+                title=book["title"], index=f"./#{bid}")
     pages = [
         dict(
             title=book["title"],
@@ -271,42 +293,45 @@ for progress, book in enumerate(books):
     pages[-1]["next"] = "#done"
     view["pages"] = pages
     view["bid"] = bid
-    view["css"] = osp.relpath(osp.join(OUT, cp.copy("book.css")), osp.dirname(bpath))
-    view["js"] = osp.relpath(osp.join(OUT, cp.link("book.js")), osp.dirname(bpath))
+    view["css"] = osp.relpath(book_css, osp.dirname(bpath))
+    view["js"] = osp.relpath(book_js, osp.dirname(bpath))
+
     html = render(template, view)
-    os.makedirs(osp.dirname(bpath), exist_ok=True)
+    makedirs(osp.dirname(bpath), exist_ok=True)
     with open(bpath, "wt", encoding="utf-8") as fp:
         fp.write(html)
 
-print("last reviewed", lastReviewed)
+print("Last Reviewed", lastReviewed)
 
 # write the index.htmls
-itemplate = open("src/book-index.mako").read()
-ipaths = sorted(set(b["path"] for b in ndx))
+idxtemplate = open("src/book-index.mako").read()
+idxpaths = sorted(set(b["path"] for b in ndx))
 start = osp.join(CONTENT, "index.html")
 back = start
 i = 1
-for path, group in itertools.groupby(ndx, lambda v: v["path"]):
+for path, group in itertools_groupby(ndx, lambda v: v["path"]):
     view = dict(
         name="index",
         books=group,
         back=osp.relpath(back, osp.dirname(path)),
-        next=osp.relpath(ipaths[i] if i < len(ipaths) else start, osp.dirname(path)),
+        next=osp.relpath(idxpaths[i] if i < len(idxpaths)
+                         else start, osp.dirname(path)),
         css=osp.relpath(osp.join(OUT, "index.css"), path),
     )
     with open(path, "wt", encoding="utf-8") as fp:
-        fp.write(render(itemplate, view))
+        fp.write(render(idxtemplate, view))
     back = path
     i += 1
 
 # write the word indexes
 WOUT = osp.join(CONTENT, "index")
-os.makedirs(WOUT, exist_ok=True)
+makedirs(WOUT, exist_ok=True)
 
-for word in wordToSlugs.keys():
+for word, slugs in wordToSlugs.iteritems():
     if len(word) < 3:
         continue
-    ids = sorted([bookmap[slug][0] for slug in wordToSlugs[word]])
+    # bookmap[slug] is a tuple whose structure is (book ID, book path)
+    ids = sorted([bookmap[slug][0] for slug in slugs])
     with open(osp.join(WOUT, word), "wt", encoding="utf-8") as fp:
         fp.write("".join(ids))
 
@@ -316,11 +341,12 @@ with open(osp.join(WOUT, "CAUTION"), "at", encoding="utf-8") as fp:
 
 # write the AllAvailable file
 with open(osp.join(WOUT, "AllAvailable"), "wt", encoding="utf-8") as fp:
+    # first-last
     fp.write("%s-%s" % ("0" * Dbooks, last))
 
 # write out a list of the images for possible prefetch...
 with open(osp.join(CONTENT, "images.json"), "wt", encoding="utf-8") as fp:
-    json.dump([osp.relpath(path, OUT) for path in imagemap.values()], fp)
+    json_dump([osp.relpath(path, OUT) for path in imagemap.values()], fp)
 
 # record parameters needed by the js
 config = {
@@ -330,5 +356,6 @@ config = {
     "lastReviewed": lastReviewed,
     "last": last,
 }
+print (f'Configuration Parameters: {config}')
 with open(osp.join(CONTENT, "config.json"), "wt") as fp:
-    json.dump(config, fp)
+    json_dump(config, fp)
